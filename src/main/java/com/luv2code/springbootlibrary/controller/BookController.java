17bb3b4b92414770b9be77b9b356cbd3efa466ae
Syamil Zamzuri <muhammadsyamil@milradius.com.my>
2023-04-15 23:56:34 +0800

    Add Okta props + Extract JWT + Add CORS for APIs

--- src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
+++ src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
@@ -2,6 +2,7 @@ package com.luv2code.springbootlibrary.controller;
 
 import com.luv2code.springbootlibrary.entity.Book;
 import com.luv2code.springbootlibrary.service.BookService;
+import com.luv2code.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,18 +16,20 @@ public class BookController {
         this.bookService = bookService;
     }
     @GetMapping("/secure/currentloans/count")
-    public int currentLoansCount()throws Exception{
-        String userEmail = "testaccount@email.com";
+    public int currentLoansCount(@RequestHeader(value = "Authorization")String token)throws Exception{
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.currentLoansCount(userEmail);
     }
     @GetMapping("/secure/ischeckout/byuser")
-    public Boolean checkoutBookByUser(@RequestParam Long bookId)throws Exception{
-        String userEmail = "testaccount@email.com";
+    public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization")String token,
+                                      @RequestParam Long bookId)throws Exception{
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBookByUser(userEmail, bookId);
     }
     @PutMapping("/secure/checkout")
-    public Book checkoutBook (@RequestParam Long bookId)throws Exception{
-        String userEmail = "testaccount@email.com";
+    public Book checkoutBook (@RequestHeader(value = "Authorization")String token,
+                              @RequestParam Long bookId)throws Exception{
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBook(userEmail, bookId);
     }
 }
